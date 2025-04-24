fedac045015002a1143d6d8c7186355704c9e0c3
JP Vernon <jp.vernon3@gmail.com>
2025-04-24 17:33:29 -0600

    Prework Objectives 1-3

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "JP Vernon";
+		int age = 35;
+		String state = "Texas";
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+	}
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int birth = 1990;
+		String month = "April";
+		boolean weekend = false;
+
+		System.out.println(birth);
+		System.out.println(month);
+		System.out.println(weekend);
+	}
+}
