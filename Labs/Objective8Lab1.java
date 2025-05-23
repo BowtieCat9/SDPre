1fbd906b5cce05436d8408deea6f121f8469db48
JP Vernon <jp.vernon3@gmail.com>
2025-05-22 19:25:58 -0600

    Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+	public static void main(String [] args) {
+		int num1;
+		int num2;
+
+		for (num1 = 1; num1 <= 10; num1++) {
+			
+			System.out.println(num1);
+		}
+		for (num2 =10; num2 >=1; num2--) {
+
+			System.out.println(num2);
+		}
+	}
+}
