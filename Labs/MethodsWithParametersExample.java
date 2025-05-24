f07df31149a4f3fc115640633b194b53a6179797
JP Vernon <jp.vernon3@gmail.com>
2025-05-23 21:17:41 -0600

    MethodsWithParametersExampleUpdated

--- Labs/MethodsWithParametersExample.java
+++ Labs/MethodsWithParametersExample.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class MethodsWithParametersExample {
 	public static void main(String [] args) {
 		Scanner kb = new Scanner(System.in);
-		String item;
-		int quantity;
-		System.out.println("Please enter your inventory item: ");
-		item = kb.nextLine();
-		System.out.println("Please enter the quantity: ");
-		quantity = kb.nextInt();
+		double num1, num2;
+		System.out.println("Please enter one decimal number: ");
+		num1 = kb.nextDouble();
+		System.out.println("Please enter another decimal number: ");
+		num2 = kb.nextDouble();
 
-		printInventory(item, quantity);
+		double answer = findSum(num1, num2);
+		System.out.println(num1 + " + " + num2 + " = " + answer);
 
 
 	}
-	public static void printInventory(String inventoryItem, int numberOfInventoryItems) {
-		System.out.println("There are currently " + numberOfInventoryItems + " " + inventoryItem + " left.");
+	public static double findSum(double x, double y) {
+		double sum = x + y;
+		return sum;
 	}
 }
